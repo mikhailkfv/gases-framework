1ff503a36bff6bf8b406f582097ace88ad9027b6
JamiesWhiteShirt <erlend@aamdal.com>
2015-03-11 20:33:01 +0100

    Added missing gas lantern type

--- src/main/java/glenn/gasesframework/api/GasesFrameworkAPI.java
+++ src/main/java/glenn/gasesframework/api/GasesFrameworkAPI.java
@@ -89,7 +89,8 @@ public class GasesFrameworkAPI
 		new LanternType("gas_1", 1.0f, "gasesframework:lantern_gas_1", new ItemKey(Items.glass_bottle), lanternTypeGasEmpty, 0),
 		new LanternType("gas_2", 1.0f, "gasesframework:lantern_gas_2", new ItemKey(Items.glass_bottle), lanternTypeGasEmpty, 0),
 		new LanternType("gas_3", 1.0f, "gasesframework:lantern_gas_3", new ItemKey(Items.glass_bottle), lanternTypeGasEmpty, 0),
-		new LanternType("gas_4", 1.0f, "gasesframework:lantern_gas_4", new ItemKey(Items.glass_bottle), lanternTypeGasEmpty, 0)
+		new LanternType("gas_4", 1.0f, "gasesframework:lantern_gas_4", new ItemKey(Items.glass_bottle), lanternTypeGasEmpty, 0),
+		new LanternType("gas_5", 1.0f, "gasesframework:lantern_gas_5", new ItemKey(Items.glass_bottle), lanternTypeGasEmpty, 0)
 	};
 	
 	/**
