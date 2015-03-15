8991b1d2ab6c59f9bce470184ff44b6ab3fbf916
JamiesWhiteShirt <erlend@aamdal.com>
2015-03-15 20:02:18 +0100

    Highly improved the configurations

--- src/main/java/glenn/moddingutils/Configurations.java
+++ src/main/java/glenn/moddingutils/Configurations.java
@@ -1,6 +1,8 @@
 package glenn.moddingutils;
 
 import java.io.File;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.reflect.Field;
 
 import net.minecraft.item.Item;
@@ -9,12 +11,20 @@ import net.minecraftforge.common.config.Configuration;
 
 /**
  * An abstract class that uses java reflection to automate the configuration setting and getting.
- * Each contained field must have a default value set statically.
  * @author Glenn
  *
  */
 public abstract class Configurations
 {
+	@Retention(RetentionPolicy.RUNTIME)
+	protected @interface ConfigField
+	{
+		public String path();
+		public String comment() default "";
+		public String defaultValue() default "";
+		public boolean autoReset() default false;
+	}
+	
 	public class ItemRepresentation
 	{
 		public String name;
@@ -29,58 +39,172 @@ public abstract class Configurations
 	
 	public Configurations(File configurationsFile)
 	{
-		setDefaults();
-		
 		Configuration config = new Configuration(configurationsFile);
 		config.load();
 		
 		for(Field field : getClass().getFields())
 		{
-			String[] splitString = field.getName().split("_");
-			if(splitString.length == 2)
+			ConfigField configField = field.getAnnotation(ConfigField.class);
+			if(configField != null)
 			{
-				String category = splitString[0];
-				String name = splitString[1];
+				String fieldPath = configField.path();
+				int lastDot = fieldPath.lastIndexOf('.');
+				String category = lastDot != -1 ? fieldPath.substring(0, lastDot) : "other";
+				String name = fieldPath.substring(lastDot + 1);
+				String comment = configField.comment();
+				
+				String defaultValue = configField.defaultValue();
 				
 				try
 				{
-					Object o = field.get(this);
+					Class<?> c = field.getType();
+					Object fieldValue = null;
 					
-					Class<?> c = o.getClass();
-					if(c == Integer.class)
+					if(c == int.class)
 					{
-						int def = (Integer)o;
-						field.set(this, new Integer(config.get(category, name, def).getInt(def)));
+						int def = 0;
+						try
+						{
+							def = Integer.parseInt(defaultValue);
+						}
+						catch(Exception e)
+						{
+							throw new RuntimeException("Invalid default value for field " + fieldPath);
+						}
+						fieldValue = new Integer(config.get(category, name, def, comment).getInt(def));
 					}
-					else if(c == Float.class)
+					else if(c == float.class)
 					{
-						float def = (Float)o;
-						field.set(this, new Float(Float.parseFloat(config.get(category, name, def).getString())));
+						float def = 0.0f;
+						try
+						{
+							def = Float.parseFloat(defaultValue);
+						}
+						catch(Exception e)
+						{
+							throw new RuntimeException("Invalid default value for field " + fieldPath);
+						}
+						fieldValue = new Float(Float.parseFloat(config.get(category, name, def, comment).getString()));
 					}
-					else if(c == Double.class)
+					else if(c == double.class)
 					{
-						double def = (Double)o;
-						field.set(this, new Double(Double.parseDouble(config.get(category, name, def).getString())));
+						double def = 0.0D;
+						try
+						{
+							def = Double.parseDouble(defaultValue);
+						}
+						catch(Exception e)
+						{
+							throw new RuntimeException("Invalid default value for field " + fieldPath);
+						}
+						fieldValue = new Double(Double.parseDouble(config.get(category, name, def, comment).getString()));
 					}
-					else if(c == Boolean.class)
+					else if(c == boolean.class)
 					{
-						boolean def = (Boolean)o;
-						field.set(this, new Boolean(config.get(category, name, def).getBoolean(def)));
+						boolean def = false;
+						try
+						{
+							def = Boolean.parseBoolean(defaultValue);
+						}
+						catch(Exception e)
+						{
+							throw new RuntimeException("Invalid default value for field " + fieldPath);
+						}
+						fieldValue = new Boolean(config.get(category, name, def, comment).getBoolean(def));
 					}
 					else if(c == String.class)
 					{
-						String def = (String)o;
-						field.set(this, config.get(category, name, def).getString());
+						fieldValue = config.get(category, name, defaultValue, comment).getString();
 					}
-					else if(c == String[].class)
+					else if(c.isArray())
 					{
-						String[] def = (String[])o;
-						field.set(this, config.get(category, name, def).getStringList());
+						Class<?> arrayC = c.getComponentType();
+						String[] defStrings = defaultValue.equals("") ? new String[0] : defaultValue.split("\n");
+						String[] stringValues = config.getStringList(name, category, defStrings, comment);
+						
+						if(arrayC == Integer.class)
+						{
+							int[] typeValues = new int[stringValues.length];
+							for(int i = 0; i < stringValues.length; i++)
+							{
+								try
+								{
+									typeValues[i] = Integer.parseInt(stringValues[i]);
+								}
+								catch(Exception e)
+								{
+									
+									typeValues[i] = 0;
+								}
+							}
+							fieldValue = typeValues;
+						}
+						else if(arrayC == Float.class)
+						{
+							float[] typeValues = new float[stringValues.length];
+							for(int i = 0; i < stringValues.length; i++)
+							{
+								try
+								{
+									typeValues[i] = Float.parseFloat(stringValues[i]);
+								}
+								catch(Exception e)
+								{
+									typeValues[i] = 0.0f;
+								}
+							}
+							fieldValue = typeValues;
+						}
+						else if(arrayC == Double.class)
+						{
+							double[] typeValues = new double[stringValues.length];
+							for(int i = 0; i < stringValues.length; i++)
+							{
+								try
+								{
+									typeValues[i] = Double.parseDouble(stringValues[i]);
+								}
+								catch(Exception e)
+								{
+									typeValues[i] = 0.0D;
+								}
+							}
+							fieldValue = typeValues;
+						}
+						else if(arrayC == Boolean.class)
+						{
+							boolean[] typeValues = new boolean[stringValues.length];
+							for(int i = 0; i < stringValues.length; i++)
+							{
+								try
+								{
+									typeValues[i] = Boolean.parseBoolean(stringValues[i]);
+								}
+								catch(Exception e)
+								{
+									typeValues[i] = false;
+								}
+							}
+							fieldValue = typeValues;
+						}
+						else if(arrayC == String.class)
+						{
+							fieldValue = stringValues;
+						}
+					}
+					
+					if(fieldValue != null)
+					{
+						field.set(this, fieldValue);
+					}
+					else
+					{
+						throw new RuntimeException("Failed to read config field " + fieldPath + " because of unknown value type " + c.getName());
 					}
 				}
-				catch (Exception e)
+				catch(Exception e)
 				{
-					System.out.print("Failed to set value for configuration field " + category + ":" + name);
+					System.out.println(e.toString() + " Failed to set value for configuration field " + fieldPath);
 				}
 			}
 		}
@@ -89,8 +213,6 @@ public abstract class Configurations
 		
 		onLoaded();
 	}
-	
-	protected abstract void setDefaults();
 	
 	protected abstract void onLoaded();
 }
