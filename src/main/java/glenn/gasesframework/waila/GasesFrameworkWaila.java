a975434ad5fca55c9b9c3ed05fbd2f284959cba1
JamiesWhiteShirt <erlend@aamdal.com>
2015-03-20 17:05:34 +0100

    Added WAILA information to lanterns

--- src/main/java/glenn/gasesframework/waila/GasesFrameworkWaila.java
+++ src/main/java/glenn/gasesframework/waila/GasesFrameworkWaila.java
@@ -3,11 +3,13 @@ package glenn.gasesframework.waila;
 import glenn.gasesframework.common.block.BlockGasPipe;
 import glenn.gasesframework.common.block.BlockGasPump;
 import glenn.gasesframework.common.block.BlockGasTank;
+import glenn.gasesframework.common.block.BlockLantern;
 import cpw.mods.fml.common.event.FMLInterModComms;
 import mcp.mobius.waila.api.IWailaRegistrar;
 
 public class GasesFrameworkWaila
 {
+	private static final LanternProvider lanternProvider = new LanternProvider();
 	private static final PipeProvider pipeProvider = new PipeProvider();
 	private static final PumpProvider pumpProvider = new PumpProvider();
 	private static final TankProvider tankProvider = new TankProvider();
@@ -16,6 +18,7 @@ public class GasesFrameworkWaila
 	public static void callbackRegister(IWailaRegistrar registrar)
 	{
 		wailaRegistrar = registrar;
+		wailaRegistrar.registerBodyProvider(lanternProvider, BlockLantern.class);
 		wailaRegistrar.registerBodyProvider(pipeProvider, BlockGasPipe.class);
 		wailaRegistrar.registerBodyProvider(pumpProvider, BlockGasPump.class);
 		wailaRegistrar.registerBodyProvider(tankProvider, BlockGasTank.class);
---
+++ src/main/java/glenn/gasesframework/waila/LanternProvider.java
@@ -0,0 +1,56 @@
+package glenn.gasesframework.waila;
+
+import glenn.gasesframework.common.block.BlockLantern;
+
+import java.util.List;
+
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+
+public class LanternProvider implements IWailaDataProvider
+{
+	@Override
+	public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config)
+	{
+		return null;
+	}
+
+	@Override
+	public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+	{
+		return currenttip;
+	}
+
+	@Override
+	public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+	{
+		BlockLantern lanternBlock = (BlockLantern)accessor.getBlock();
+		if(lanternBlock.type.expires())
+		{
+			float percentage = 100.0f * (1.0f - accessor.getMetadata() / 16.0f);
+			currenttip.add(String.format("%.1f%%", percentage));
+		}
+		
+		
+		return currenttip;
+	}
+
+	@Override
+	public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config)
+	{
+		return currenttip;
+	}
+
+	@Override
+	public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, int x, int y, int z)
+	{
+		return tag;
+	}
+}
