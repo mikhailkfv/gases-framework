d6ed8a85edf4d1000487b0564744dbcfc2311daf
JamiesWhiteShirt <erlend@aamdal.com>
2015-03-22 10:21:51 +0100

    Added documentation to IGasEffectProtector

--- src/main/java/glenn/gasesframework/api/item/IGasEffectProtector.java
+++ src/main/java/glenn/gasesframework/api/item/IGasEffectProtector.java
@@ -4,8 +4,26 @@ import glenn.gasesframework.api.gastype.GasType;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 
+/**
+ * An interface for items that can protect against gas effects when worn.
+ * @author Glenn
+ */
 public interface IGasEffectProtector
 {
+	/**
+	 * Return true if this item will protect the vision of the holder.
+	 * @param entity
+	 * @param stack
+	 * @param type
+	 * @return
+	 */
 	public boolean protectVision(EntityLivingBase entity, ItemStack stack, GasType type);
+	/**
+	 * Return true if this item will protect the breath of the holder.
+	 * @param entity
+	 * @param stack
+	 * @param type
+	 * @return
+	 */
 	public boolean protectBreath(EntityLivingBase entity, ItemStack stack, GasType type);
 }
