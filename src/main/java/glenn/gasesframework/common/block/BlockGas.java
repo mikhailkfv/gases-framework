37373bc4caaca35bcfd5c5352abf9d407b4fbbc5
Erlend Åmdal <erlend@aamdal.com>
2015-07-22 00:14:36 +0200

    Make dropped items ignite gas if registered

--- src/main/java/glenn/gasesframework/common/block/BlockGas.java
+++ src/main/java/glenn/gasesframework/common/block/BlockGas.java
@@ -14,8 +14,10 @@ import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.Explosion;
@@ -107,6 +109,13 @@ public class BlockGas extends Block implements ISample
     				setOnFire |= GasesFrameworkAPI.isIgnitionItem(stack.getItem());
     			}
     		}
+    	}
+    	else if(entity instanceof EntityItem)
+    	{
+    		EntityItem itemEntity = (EntityItem)entity;
+    		
+    		Item item = itemEntity.getEntityItem().getItem();
+    		setOnFire = GasesFrameworkAPI.isIgnitionItem(item);
     	}
     	
     	if(setOnFire)
